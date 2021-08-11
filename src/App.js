69a5f5c0a64bfbaf5eb08f51b82e5688a292696d
padmanabh ganorkar <padmanabh_ganorkar@persistent.com>
2021-08-11 18:06:25 +0530

    initial implementation

--- src/App.js
+++ src/App.js
@@ -2,50 +2,124 @@
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
 import TimerLengthControl from './TimerLengthControl'
+import { useState, useEffect } from 'react';
 
 function App() {
+  const accurateInterval = function (fn, time) {
+    var cancel, nextAt, timeout, wrapper;
+    nextAt = new Date().getTime() + time;
+    timeout = null;
+    wrapper = function () {
+      nextAt += time;
+      timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+      return fn();
+    };
+    cancel = function () {
+      return clearTimeout(timeout);
+    };
+    timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+    return {
+      cancel: cancel
+    };
+  };
+
+  const CONSTANTS = {
+    SESSION: "SESSION",
+    BREAK: "BREAK",
+    SESSION_LENGTH: 60,
+    BREAK_LENGTH: 60
+  }
+  const initialState = {
+    breakLength: 5,
+    sessionLength: 25,
+    timerState: CONSTANTS.STOPPED,
+    timerType: CONSTANTS.SESSION,
+    timeLimit: 25 * 60,
+    timerRunning: false,
+    timeout: null
+  }
+  const [state, setState] = useState(initialState)
+  const handleDecrement = (mode) => {
+    if (!state.timerRunning) {
+      const newSessionLength = mode === CONSTANTS.SESSION ? state.sessionLength > 1 ? state.sessionLength - 1 : 1 : state.sessionLength;
+      const newBreakLength = mode === CONSTANTS.BREAK ? state.breakLength > 1 ? state.breakLength - 1 : 1 : state.breakLength;
+      const newTimeLimit = state.timerType === CONSTANTS.SESSION ? newSessionLength * 60 : newBreakLength * 60;
+      setState(prev => ({
+        ...prev, sessionLength: newSessionLength, breakLength: newBreakLength, timeLimit: newTimeLimit
+      }))
+    }
+  }
+  const handleIncrement = (mode) => {
+    if (!state.timerRunning) {
+      const newSessionLength = mode === CONSTANTS.SESSION ? state.sessionLength < CONSTANTS.SESSION_LENGTH ? state.sessionLength + 1 : CONSTANTS.SESSION_LENGTH : state.sessionLength;
+      const newBreakLength = mode === CONSTANTS.BREAK ? state.breakLength < CONSTANTS.BREAK_LENGTH ? state.breakLength + 1 : CONSTANTS.BREAK_LENGTH : state.breakLength;
+      const newTimeLimit = state.timerType === CONSTANTS.SESSION ? newSessionLength * 60 : newBreakLength * 60;
+      setState(prev => ({
+        ...prev, sessionLength: newSessionLength, breakLength: newBreakLength, timeLimit: newTimeLimit
+      }))
+    }
+  }
+  const handleReset = () => {
+    setState(initialState);
+    const beep = document.getElementById("beep");
+    beep.pause();
+    beep.currentTime = 0;
+  }
+
+  //let timeout = null;
+  const handleStopStart = () => {
+    setState(prev => ({
+      ...prev, timerRunning: !prev.timerRunning
+    }))
+  }
+
+  const getClock = () => {
+    let minutes = Math.floor(state.timeLimit / 60);
+    let seconds = state.timeLimit - minutes * 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return minutes + ':' + seconds;
+  }
+
   return (
     <div id="container">
-        <div className="main-title">25 + 5 Clock</div>
-        <div>
+      <div className="main-title">25 + 5 Clock</div>
+      <div>
         <TimerLengthControl
-          addID="break-increment"
-          length={5}
-          lengthID="break-length"
-          minID="break-decrement"
-          onClick={console.log("setBreakLength")}
+          length={state.breakLength}
+          onDecrement={handleDecrement}
+          onIncrement={handleIncrement}
           title="Break Length"
-          titleID="break-label"
+          mode={CONSTANTS.BREAK}
         />
         <TimerLengthControl
-          addID="session-increment"
-          length={25}
-          lengthID="session-length"
-          minID="session-decrement"
-          onClick={console.log("setSessionLength")}
+          length={state.sessionLength}
+          onDecrement={handleDecrement}
+          onIncrement={handleIncrement}
           title="Session Length"
-          titleID="session-label"
-        /></div>
-        <div className="timer">
-          <div className="timer-wrapper">
-            <div id="timer-label">test</div>
-            <div id="time-left">test</div>
-          </div>
-        </div>
-        <div className="timer-control">
-          <button id="start_stop" onClick={console.log("start/stop")}>
-            <i className="fa fa-play fa-2x" />
-            <i className="fa fa-pause fa-2x" />
-          </button>
-          <button id="reset" onClick={console.log("reset")}>
-            <i className="fa fa-refresh fa-2x" />
-          </button>
-        </div>
-        <audio
-          id="beep"
-          preload="auto"
-          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+          mode={CONSTANTS.SESSION}
         />
+      </div>
+      <div className="timer">
+        <div className="timer-wrapper">
+          <div id="timer-label" className={state.timeLimit < 60 ? "warning" : ""}>{state.timerType}</div>
+          <div id="time-left" className={state.timeLimit < 60 ? "warning" : ""}>{getClock()}</div>
+        </div>
+      </div>
+      <div className="timer-control">
+        <button id="start_stop" onClick={handleStopStart}>
+          <i className="fa fa-play fa-2x" />
+          <i className="fa fa-pause fa-2x" />
+        </button>
+        <button id="reset" onClick={handleReset}>
+          <i className="fa fa-refresh fa-2x" />
+        </button>
+      </div>
+      <audio
+        id="beep"
+        preload="auto"
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      />
     </div>
   );
 }
--- src/TimerLengthControl.js
+++ src/TimerLengthControl.js
@@ -3,23 +3,23 @@ import React from 'react'
 const TimerLengthControl = (props) => {
     return (
         <div className="length-control">
-            <div id={props.titleID}>{props.title}</div>
+            <div id={props.mode.toLowerCase()+"-label"} >{props.title}</div>
             <button
                 className="btn-level"
-                id={props.minID}
-                onClick={props.onClick}
+                onClick={()=>props.onDecrement(props.mode)}
                 value="-"
+                id={props.mode.toLowerCase()+"-decrement"}
             >
                 <i className="fa fa-arrow-down fa-2x" />
             </button>
-            <div className="btn-level" id={props.lengthID}>
+            <div className="btn-level" id={props.mode.toLowerCase()+"-length"}>
                 {props.length}
             </div>
             <button
                 className="btn-level"
-                id={props.addID}
-                onClick={props.onClick}
+                onClick={()=>props.onIncrement(props.mode)}
                 value="+"
+                id={props.mode.toLowerCase()+"-increment"}
             >
                 <i className="fa fa-arrow-up fa-2x" />
             </button>
